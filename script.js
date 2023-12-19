5c7263a5e5b9346eccd5fd8b17f8d060110ca9d9
ChenZakaim <chenzakaim@hilma.tech>
2023-12-19 12:09:53 +0200

    change the function fooBar

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function fooBar() {
-	let counter = -1;
+function fooBar(num) {
+  let counter = -1;
 
-	while (counter < 10) {
-		console.log("count is " + counter);
-		counter += 2;
-	}
+  while (counter < 10) {
+    console.log("count is " + counter);
+    counter += 2;
+  }
+  return num;
 }
 
-fooBar();
+fooBar(2);
 
 if (new Date().getMonth() === 20) {
-	localStorage.removeItem("user");
+  localStorage.removeItem("user");
 }
 
 document.getElementsByTagName("button")[0].addEventListener("click", () => {
-	alert("hi");
+  alert("hi");
 });
